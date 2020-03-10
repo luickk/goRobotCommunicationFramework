b1e5f3000ed5b4dae13b1fc4a6a0ce1bf9fc322b
luick klippel <gelberdev@gmail.com>
2020-03-10 09:37:46 +0100

    creating node instance object

--- rcf_node/rcf_node.go
+++ rcf_node/rcf_node.go
@@ -10,6 +10,7 @@ Robot Communication Framework
 package rcf_node
 
 import (
+  "os"
   "fmt"
 	"bufio"
 	"net"
@@ -23,8 +24,35 @@ var topic_capacity = 5
 // general service function type
 type service_fn func ()
 
+// node struct
+type node struct {
+  // id or port of node
+  id int
+
+  // key: topic name, value: stack slice
+  topics map[string][]string
+
+  // channel map with first key(topic name) value(msg, topic element) pair, whichs element is then pushed to topic with topic name
+  topic_push_ch chan map[string]string
+
+  // channel map with first key(topic name) value(msg, topic element) pair, whichs element is then pushed to topic with topic name
+  topic_init_ch chan string
+
+  // channel map with first key(topic name) value(listening conn) pair, that's then added to listener conn
+  topic_listener_conn_ch chan map[net.Conn]string
+
+  // service map with first key(service name) value(anon service func) pair
+  services map[string]service_fn
+
+  // service map with first key(service name) value(anon service func) that's then added to services map
+  service_init_ch chan map[string]service_fn
+
+  // string channel with each string representing a service name executed when pushed to channel
+  service_exec_ch chan string
+}
+
 // handles every incoming node client connection
-func handle_Connection(topic_push_ch chan <- map[string]string, topic_init_ch chan string, topic_listener_conn_ch chan <- map[net.Conn]string, service_exec_ch chan string, conn net.Conn, topics map[string][]string) {
+func handle_Connection(node node, conn net.Conn) {
   defer conn.Close()
 
   for {
@@ -44,7 +72,7 @@ func handle_Connection(topic_push_ch chan <- map[string]string, topic_init_ch ch
         conn.Close()
         return
       } else if rcf_util.Trim_suffix(data) =="list_topics" {
-        conn.Write([]byte(strings.Join(Node_list_topics(topics), ",")+"\n"))
+        conn.Write([]byte(strings.Join(Node_list_topics(node), ",")+"\n"))
       }
 
       // cmds with args/ require parsing
@@ -55,24 +83,24 @@ func handle_Connection(topic_push_ch chan <- map[string]string, topic_init_ch ch
       if len(push_rdata)>=2 && string(data[0])!="+" {
         topic_name := push_rdata[0]
         tdata := push_rdata[1]
-        Topic_push_data(topic_push_ch, topics, topic_name, tdata)
+        Topic_push_data(node, topic_name, tdata)
 
       // data pulled from stack
       } else if len(pull_rdata) >=2 && string(data[0])!="+" {
         topic_name := pull_rdata[0]
         elements,_ := strconv.Atoi(rcf_util.Trim_suffix(pull_rdata[1]))
-        conn.Write([]byte(strings.Join(Topic_pull_data(topics, topic_name, elements), ",")+"\n"))
+        conn.Write([]byte(strings.Join(Topic_pull_data(node, topic_name, elements), ",")+"\n"))
 
       } else if string(data[0])=="+" {
-        Topic_init(topic_init_ch, data)
+        Topic_init(node, data)
 
       // $ enables continuous data streaming mode, in whichthe topics data is continuously send to the client
       } else if string(data[0])=="$" {
         topic_name := rcf_util.Apply_naming_conv(data)
-        Topic_add_listener_conn(topics, topic_listener_conn_ch, topic_name, conn)
+        Topic_add_listener_conn(node, topic_name, conn)
       } else if string(data[0])=="*" {
         exec_service_name := rcf_util.Apply_naming_conv(data)
-        Service_exec(service_exec_ch, exec_service_name)
+        Service_exec(node, exec_service_name)
       }
       // fmt.Println(topics)
       data = ""
@@ -82,27 +110,27 @@ func handle_Connection(topic_push_ch chan <- map[string]string, topic_init_ch ch
 
 // handles all memory critical write operations to topic map and
 // reduces the topics slice to given max length
-func topic_handler(topic_push_ch <- chan map[string]string, topic_init_ch <- chan string, topic_listener_conn_ch <- chan map[net.Conn]string, topics map[string][]string, topic_capacity int) {
+func topic_handler(node node) {
   listener_conns := make(map[net.Conn]string)
   for {
     select {
 
-      case listener_topic_map := <-topic_listener_conn_ch:
+    case listener_topic_map := <-node.topic_listener_conn_ch:
         listening_conn := rcf_util.Get_first_map_key_cs(listener_topic_map)
         listener_conns[listening_conn] = listener_topic_map[listening_conn]
-      case topic_element_map := <-topic_push_ch:
+      case topic_element_map := <-node.topic_push_ch:
         topic_name := rcf_util.Get_first_map_key_ss(topic_element_map)
 
-        if rcf_util.Topics_contains_topic(topics, topic_name){
+        if rcf_util.Topics_contains_topic(node.topics, topic_name){
           topic_val_element := topic_element_map[topic_name]
 
-          topics[topic_name] = append(topics[topic_name], topic_val_element)
+          node.topics[topic_name] = append(node.topics[topic_name], topic_val_element)
 
           // check of topic exceeds topic cap limits
-          if len(topics[topic_name]) > topic_capacity {
-            topic_overhead := len(topics[topic_name])-topic_capacity
+          if len(node.topics[topic_name]) > topic_capacity {
+            topic_overhead := len(node.topics[topic_name])-topic_capacity
             // slicing size of slice to right size
-            topics[topic_name] = topics[topic_name][topic_overhead:]
+            node.topics[topic_name] = node.topics[topic_name][topic_overhead:]
           }
 
           // check if topic, which data is pushed to, has a listening conn
@@ -112,37 +140,39 @@ func topic_handler(topic_push_ch <- chan map[string]string, topic_init_ch <- cha
             }
           }
         }
-      case topic_init_name := <-topic_init_ch:
+      case topic_init_name := <- node.topic_init_ch:
         fmt.Println("+[topic] ", topic_init_name)
-        if rcf_util.Topics_contains_topic(topics, topic_init_name) {
+        if rcf_util.Topics_contains_topic(node.topics, topic_init_name) {
           fmt.Println("/[topic] ", topic_init_name)
         } else {
-          topics[topic_init_name] = []string{"init"}
+          node.topics[topic_init_name] = []string{"init"}
         }
     }
   }
 }
 
-func service_handler(service_init_ch <- chan map[string]service_fn, service_exec_ch chan string, services map[string]service_fn) {
+func service_handler(node node) {
   for {
     select {
-    case init_service_map := <-service_init_ch:
+    case init_service_map := <- node.service_init_ch:
       var init_service_name string
       for k := range init_service_map { init_service_name = k }
-      services[init_service_name] = init_service_map[init_service_name]
-    case service_exec_ch := <-service_exec_ch:
-      services[service_exec_ch]()
+      node.services[init_service_name] = init_service_map[init_service_name]
+    case service_exec_ch := <-node.service_exec_ch:
+      service_func := node.services[service_exec_ch]
+
+      service_func()
+
     default:
 
     }
   }
 }
 
-// initiating node with given id
-// returns topic init, push channel and service init, execute channel to enable direct service and topic operations
-func Init(node_id int) (chan map[string]string, chan string, chan map[string]service_fn, chan string){
-  fmt.Println("+[node] ", node_id)
 
+
+// creating node instance struct
+func Create(node_id int) node{
   // key: topic name, value: stack slice
   topics := make(map[string][]string)
 
@@ -155,17 +185,28 @@ func Init(node_id int) (chan map[string]string, chan string, chan map[string]ser
   // channel map with first key(topic name) value(listening conn) pair, that's then added to listener conn
   topic_listener_conn_ch := make(chan map[net.Conn]string)
 
-  go topic_handler(topic_push_ch, topic_init_ch, topic_listener_conn_ch, topics, topic_capacity)
-
+  // service map with first key(service name) value(anon service func) pair
   services := make(map[string]service_fn)
 
+  // service map with first key(service name) value(anon service func) that's then added to services map
   service_init_ch := make(chan map[string]service_fn)
 
+  // string channel with each string representing a service name executed when pushed to channel
   service_exec_ch := make(chan string)
 
-  go service_handler(service_init_ch, service_exec_ch, services)
+  return node{node_id, topics, topic_push_ch, topic_init_ch, topic_listener_conn_ch, services, service_init_ch, service_exec_ch}
+}
 
-  var port string = ":"+strconv.Itoa(node_id)
+// initiating node with given id
+// returns initiated node instance to enable direct service and topic operations
+func Init(node node) {
+  fmt.Println("+[node] ", node.id)
+
+  go topic_handler(node)
+
+  go service_handler(node)
+
+  var port string = ":"+strconv.Itoa(node.id)
 
   l, err := net.Listen("tcp4", port)
 
@@ -174,52 +215,54 @@ func Init(node_id int) (chan map[string]string, chan string, chan map[string]ser
   }
 
   defer l.Close()
-
   for {
     conn, err_handle := l.Accept()
     if err_handle != nil {
       fmt.Println("/[node] ",err_handle)
     }
-    go handle_Connection(topic_push_ch, topic_init_ch, topic_listener_conn_ch, service_exec_ch, conn, topics)
+    go handle_Connection(node, conn)
   }
-  return topic_push_ch, topic_init_ch, service_init_ch, service_exec_ch
 }
 
-func Topic_add_listener_conn(topics map[string][]string, topic_listener_conn_ch chan <- map[net.Conn]string, topic_name string, conn net.Conn) {
+func Node_halt() {
+  bufio.NewReader(os.Stdin).ReadBytes('\n')
+}
+
+func Topic_add_listener_conn(node node, topic_name string, conn net.Conn) {
     topic_name = rcf_util.Apply_naming_conv(topic_name)
     fmt.Println("cpull ", topic_name)
-    if rcf_util.Topics_contains_topic(topics, topic_name) {
-      topic_listener_conn_ch <- map[net.Conn]string {conn: topic_name}
+    if rcf_util.Topics_contains_topic(node.topics, topic_name) {
+      node.topic_listener_conn_ch <- map[net.Conn]string {conn: topic_name}
     }
 }
 
-func Node_list_topics(topics map[string][]string) []string{
+func Node_list_topics(node node) []string{
   fmt.Println("listing topics")
-  keys := make([]string, 0, len(topics))
-  for k, v := range topics {
+  keys := make([]string, 0, len(node.topics))
+  for k, v := range node.topics {
     v=v
     keys = append(keys, k)
   }
-  if len(topics) > 0 {
+  if len(node.topics) > 0 {
     return keys
-  } else if len(topics) == 0 {
+  } else if len(node.topics) == 0 {
     return []string{"none"}
   }
   return []string{"none"}
 }
 
-func Topic_pull_data(topics map[string][]string, topic_name string, elements int) []string {
-  if elements >= len(topics[topic_name]){
-    return topics[topic_name]
+func Topic_pull_data(node node, topic_name string, elements int) []string {
+  if elements >= len(node.topics[topic_name]){
+    return node.topics[topic_name]
   } else {
-    return topics[topic_name][:elements]
+    return node.topics[topic_name][:elements]
   }
   return []string{"none"}
 }
 
-func Topic_push_data(topic_push_ch chan<- map[string]string, topics map[string][]string, topic_name string, tdata string) {
-  if rcf_util.Topics_contains_topic(topics, topic_name) {
-    topic_push_ch <- map[string]string {topic_name: rcf_util.Trim_suffix(tdata)}
+func Topic_push_data(node node, topic_name string, tdata string) {
+  if rcf_util.Topics_contains_topic(node.topics, topic_name) {
+    node.topic_push_ch <- map[string]string {topic_name: rcf_util.Trim_suffix(tdata)}
     fmt.Println("->[topic] ", topic_name)
   } else {
     fmt.Println("/+[topic] ", topic_name)
@@ -227,17 +270,17 @@ func Topic_push_data(topic_push_ch chan<- map[string]string, topics map[string][
 }
 
 // create command&control topic
-func Topic_init(topic_init_ch chan string, topic_name string) {
+func Topic_init(node node, topic_name string) {
   topic_name = rcf_util.Apply_naming_conv(topic_name)
 
-  topic_init_ch <- topic_name
+  node.topic_init_ch <- topic_name
 }
 
-func Service_init(service_init_ch chan map[string]service_fn, service_name string, service_func service_fn) {
+func Service_init(node node, service_name string, service_func service_fn) {
     service_name = rcf_util.Apply_naming_conv(service_name)
-    service_init_ch <- map[string]service_fn {service_name: service_func}
+    node.service_init_ch <- map[string]service_fn {service_name: service_func}
 }
 
-func Service_exec(service_exec_ch chan string, service_name string) {
-    service_exec_ch <- service_name
+func Service_exec(node node, service_name string) {
+    node.service_exec_ch <- service_name
 }
--- test_node.go
+++ test_node.go
@@ -1,17 +1,18 @@
 package main
 
 import (
-  "fmt"
   rcf_node "robot-communication-framework/rcf_node"
 )
 
 func main() {
-  topic_push_ch, topic_init_ch, service_init_ch, service_exec_ch := rcf_node.Init(28)
+  node := rcf_node.Create(28)
 
-  rcf_node.Service_init(service_init_ch, "test", func(){
-    fmt.Println("---- SERVICE TEST EXECUTED")
-  })
 
-  fmt.Println(topic_push_ch, topic_init_ch, service_init_ch, service_exec_ch)
+  // rcf_node.Service_init(node, "test", func(){
+  //   fmt.Println("---- SERVICE TEST EXECUTED")
+  // })
+
+  rcf_node.Init(node)
+
 
 }
