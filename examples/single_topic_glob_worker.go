da22d45eb38fa9715198b38284bd1c36c57759ca
luick klippel <gelberdev@gmail.com>
2020-06-24 11:01:30 +0200

    fixing minor sample timing issue

--- examples/single_topic_glob_worker.go
+++ examples/single_topic_glob_worker.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 	rcfNodeClient "rcf/rcfNodeClient"
+	"time"
 )
 
 func main() {
-  // opening connection(tcp client) to node with id(port) 30
-  client := rcfNodeClient.NodeOpenConn(47)
+	// opening connection(tcp client) to node with id(port) 30
+	client := rcfNodeClient.NodeOpenConn(47)
 
-  for {
-	result := rcfNodeClient.TopicPullGlobData(client, 5, "altsensglob")
+	for {
+		result := rcfNodeClient.TopicPullGlobData(client, 5, "altsensglob")
 
-	println("glob single pull results: ")
-	fmt.Println(result)
-	time.Sleep(10000*time.Microsecond)
-    // time.Sleep(1*time.Second)
-  }
+		println("glob single pull results: ")
+		fmt.Println(result)
+		time.Sleep(10 * time.Millisecond)
+	}
 
-  // closing node conn at program end
-  rcfNodeClient.NodeCloseConn(client)
+	// closing node conn at program end
+	rcfNodeClient.NodeCloseConn(client)
 }
--- examples/single_topic_string_worker.go
+++ examples/single_topic_string_worker.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"fmt"
-	"time"
 	rcfNodeClient "rcf/rcfNodeClient"
+	"time"
 )
 
 func main() {
-  // opening connection(tcp client) to node with id(port) 30
-  client := rcfNodeClient.NodeOpenConn(47)
+	// opening connection(tcp client) to node with id(port) 30
+	client := rcfNodeClient.NodeOpenConn(47)
 
-  for {
-	result := rcfNodeClient.TopicPullStringData(client, 5, "altsensstring")
+	for {
+		result := rcfNodeClient.TopicPullStringData(client, 5, "altsensstring")
 
-	println("string single results: ")
-	fmt.Println(result)
-	time.Sleep(1000*time.Microsecond)
-    // time.Sleep(1*time.Second)
-  }
+		println("string single results: ")
+		fmt.Println(result)
+		time.Sleep(10 * time.Millisecond)
+	}
 
-  // closing node conn at program end
-  rcfNodeClient.NodeCloseConn(client)
+	// closing node conn at program end
+	rcfNodeClient.NodeCloseConn(client)
 }
